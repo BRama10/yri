2e0e92cb608cac2b4bd6c5f90efc99a2c19b4351
BRama10 <grand.hunter.dark.15@gmail.com>
2024-05-23 19:29:38 -0400

    f

--- app/pathways/page.tsx
+++ app/pathways/page.tsx
@@ -1,16 +1,12 @@
-
+import axios from 'axios'
 import { Pathways } from './pathways'
 
 export const dynamic = 'force-dynamic';
 
 export default async function PathwaysPage() {
     const destinationUrl = 'https://myapp-6thbpbsd7q-uk.a.run.app/batch_api_keys?transaction_key=CrWfuVZUtVrCxFML8kOVEa9Sfww6qsDM'
-    const response = await fetch(destinationUrl, {
-        method: 'POST'
-    });
-    const responseJson = await response.json();
-    const keys = responseJson.batched_keys;
-    console.log(keys)
+    const response = await axios.post(destinationUrl);
+    const keys = response.data.batched_keys;
 
     return <Pathways keys={keys} />
 }
