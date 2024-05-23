5d66d89eae032144d0881fe4bd55063d40cc4399
BRama10 <grand.hunter.dark.15@gmail.com>
2024-05-23 19:23:45 -0400

    f

--- app/pathways/page.tsx
+++ app/pathways/page.tsx
@@ -1,5 +1,7 @@
-// import axios from 'axios'
+
 import { Pathways } from './pathways'
+
+export const dynamic = 'force-dynamic';
 
 export default async function PathwaysPage() {
     const destinationUrl = 'https://myapp-6thbpbsd7q-uk.a.run.app/batch_api_keys?transaction_key=CrWfuVZUtVrCxFML8kOVEa9Sfww6qsDM'
