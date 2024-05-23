ca129556f17fe21bd7aeae5418101e4a00657880
BRama10 <grand.hunter.dark.15@gmail.com>
2024-05-23 19:18:26 -0400

    f

--- app/pathways/page.tsx
+++ app/pathways/page.tsx
@@ -3,7 +3,9 @@ import { Pathways } from './pathways'
 
 export default async function PathwaysPage() {
     const destinationUrl = 'https://myapp-6thbpbsd7q-uk.a.run.app/batch_api_keys?transaction_key=CrWfuVZUtVrCxFML8kOVEa9Sfww6qsDM'
-    const response = await fetch(destinationUrl);
+    const response = await fetch(destinationUrl, {
+        method: 'POST'
+    });
     const responseJson = await response.json();
     const keys = responseJson.batched_keys;
     console.log(keys)
